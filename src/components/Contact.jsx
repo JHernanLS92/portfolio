64aaad7ad5d010271db40199000b6ee7e7777702
JHernanLS <jhernan.lopez92@gmail.com>
2023-03-08 12:46:46 -0600

    Contact update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,13 +7,21 @@ const Contact = () => {
     <div className="Contact">
       <div className="ContainerContact">
         <h1>Contacto</h1>
-        <div>
-          <img src={images.gmail} alt="" />
-          <h3>jhernan.lopez92@gmail.com</h3>
-          <img src={images.whatsapp} alt="" />
-          <h3>4191126988</h3>
-          <img src={images.linkedin} alt="" />
-          <h2>Linkedin</h2>
+        <div className="infoContact">
+          <div className="cont">
+            <img src={images.gmail} alt="" />
+            <h4>jhernan.lopez92@gmail.com</h4>
+          </div>
+          <div className="cont">
+            <img src={images.whatsapp} alt="" />
+            <h4>4191126988</h4>
+          </div>
+          <div className="cont">
+            <img src={images.linkedin} alt="" />
+            <p>
+            https://www.linkedin.com/juanhernanlopezsuaste/
+              </p> 
+          </div>
         </div>
       </div>
     </div>
